7a6d25807b19edfed5de109b2a919b69a156ed17
Rahul Kumar <rahul1.shc@gmail.com>
2023-07-15 14:34:29 +0530

    first push

---
+++ http/src/httprequest.rs
@@ -0,0 +1,73 @@
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq)]
+pub enum Resource {
+    Path(String),
+}
+
+#[derive(Debug)]
+pub struct HttpRequest {
+    pub method: Method,
+    pub version: Version,
+    pub resource: Resource,
+    pub headers: HashMap<String, String>,
+    pub msg_body: String,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Method {
+    Get,
+    Post,
+    Uninitialized,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V1_1,
+    V2_0,
+    Uninitialized,
+}
+
+impl From<&str> for Method {
+    fn from(s: &str) -> Method {
+        match s {
+            "GET" => Method::Get,
+            "POST" => Method::Post,
+            _ => Method::Uninitialized,
+        }
+    }
+}
+
+impl From<&str> for Version {
+    fn from(s: &str) -> Version {
+        match s {
+            "HTTP/1.1" => Version::V1_1,
+            _ => Version::Uninitialized,
+        }
+    }
+}
+
+impl From<String> for HttpRequest {
+    fn from(req: String) -> Self {
+        let mut parsed_method = Method::Uninitialized;
+    }
+}
+
+fn process_req_line(s: &str) -> (Method, Resource, Version) {}
+fn process_heeader_line(s: &str) -> (String, String) {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_method_into() {
+        let m: Method = "GET".into();
+        assert_eq!(m, Method::Get);
+    }
+
+    #[test]
+    fn test_version_into() {
+        let m: Version = "HTTP/1.1".into();
+        assert_eq!(m, Version::V1_1);
+    }
+}
